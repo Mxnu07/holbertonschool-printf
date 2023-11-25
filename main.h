4bb9a27a07a63e965abb615a35b778785b0a09a8
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-24 20:38:38 -0400

    adding prototypes

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int printChar(va_list args);
-
+int printStr(va_list args);
+int _putchar(char c);
 #endif
