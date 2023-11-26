bb99e760dc282f622e3077449b466b9cdbc8dc20
eunielis5 <seunielis@gmail.com>
2023-11-26 10:28:05 -0400

    Merge README.md from Eunielis to main

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _printf - print to stout formatted text
+*
+* @format: format specifier
+*
+* Return: number of bytes printed
+*/
+int _printf(const char *format, ...)
+{
+	unsigned int i, count = 0
+
+}
+
