dafee286c245c69e4848d8498c15084739ffe6c0
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-26 20:56:28 -0400

    debugging function

--- _printf.c
+++ _printf.c
@@ -1,42 +1,54 @@
 #include "main.h"
+
 /**
  * _printf - custom printf function
  * @format: identifier
- * Return: lenght
+ * Return: length
  */
-
 int _printf(const char *format, ...)
 {
-	match m[] = {
-		{"%c", printChar}, {"%s", printStr}, {"%%", print37}, {"%d", print_d}, {"%i", print_i}
-	};
+    match m[] = {
+        {"%c", printChar},
+        {"%s", printStr},
+        {"%%", print37},
+        {"%d", print_d},
+        {"%i", print_i}
+    };
 
-	va_list args;
-	int i = 0;
-	int len = 0;
-	int j;
+    va_list args;
+    int i = 0;
+    int length = 0;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    va_start(args, format);
 
-here:
-	while (format[i] != '\0')
-	{
-		j = 4;
-			while (j >= 0)
-			{
-				if (m[j].id [0] == format[i] && m[j].id[1] == format[i + 1])
-				{
-					len = len + m[j].f(args);
-					goto here;
-				}
-				j --;
-			}
-		_putchar(format[i]);
-		i++;
-		len++;
-	}
-	va_end(args);
-	return (len);
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+    {
+        va_end(args);
+        return (-1);
+    }
+
+    while (format[i] != '\0')
+    {
+        int j;
+
+        for (j = 0; j < 5; j++)
+        {
+            if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+            {
+                length += m[j].f(args);
+                break;
+            }
+        }
+
+        if (j == 5)
+        {
+            _putchar(format[i]);
+            length++;
+        }
+
+        i++;
+    }
+
+    va_end(args);
+    return (length);
 }
